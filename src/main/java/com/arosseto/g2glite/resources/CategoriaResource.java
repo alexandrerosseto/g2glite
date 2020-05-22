5e83f8488651cc25de08fced70222458b9a15ca7
alexandrerosseto <alexandrerosseto@gmail.com>
2020-05-22 16:37:45 -0300

    Testing first entity - Categoria

--- src/main/java/com/arosseto/g2glite/resources/CategoriaResource.java
+++ src/main/java/com/arosseto/g2glite/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.arosseto.g2glite.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.arosseto.g2glite.entities.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@GetMapping
-	public String listar() {
-		return "Rest";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>(Arrays.asList(cat1, cat2));
+		
+		return lista;
 	}
 }
