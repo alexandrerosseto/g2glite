751df07ccd61e0d6e82a8001fb8103d4c0c03fc7
alexandrerosseto <alexandrerosseto@gmail.com>
2020-06-10 15:23:47 -0300

    Deleting one category with DELETE

--- src/main/java/com/arosseto/g2glite/services/CategoryService.java
+++ src/main/java/com/arosseto/g2glite/services/CategoryService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.arosseto.g2glite.entities.Category;
 import com.arosseto.g2glite.repositories.CategoryRepository;
+import com.arosseto.g2glite.services.exceptions.DatabaseException;
 import com.arosseto.g2glite.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -33,5 +36,18 @@ public class CategoryService {
 	public Category update(Category obj) {
 		findById(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Long id) {
+		findById(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 }
