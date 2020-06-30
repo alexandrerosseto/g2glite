71b561b3a1d83c05d67e2ce4368b5f7b88e43b0f
alexandrerosseto <alexandrerosseto@gmail.com>
2020-06-30 20:55:21 -0300

    Added Authentication and token JWT generation

---
+++ src/main/java/com/arosseto/g2glite/services/UserDetailServiceImpl.java
@@ -0,0 +1,29 @@
+package com.arosseto.g2glite.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.arosseto.g2glite.entities.Client;
+import com.arosseto.g2glite.repositories.ClientRepository;
+import com.arosseto.g2glite.security.UserAuth;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClientRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Client clt = repo.findByEmail(email);
+		if (clt == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserAuth(clt.getId(), clt.getEmail(), clt.getPassword(), clt.getProfile());
+	}
+
+}
