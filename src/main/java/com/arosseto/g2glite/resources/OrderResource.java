a2f12f45ca6c09e2a197c7f3582c1e64244a13f6
alexandrerosseto <alexandrerosseto@gmail.com>
2020-07-08 22:23:48 -0300

    Content restriction: Client only retrive its orders

--- src/main/java/com/arosseto/g2glite/resources/OrderResource.java
+++ src/main/java/com/arosseto/g2glite/resources/OrderResource.java
@@ -1,17 +1,18 @@
 package com.arosseto.g2glite.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -25,12 +26,6 @@ public class OrderResource {
 	@Autowired
 	private OrderService service;
 	
-	@GetMapping
-	public ResponseEntity<List<Order>> findAll() {
-		List<Order> listObj = service.findAll();
-		return ResponseEntity.ok().body(listObj);
-	}
-	
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Order> findById(@PathVariable Long id) {
 		Order obj = service.findById(id);
@@ -42,5 +37,15 @@ public class OrderResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<Order>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue="moment") String orderBy,
+			@RequestParam(value="direction", defaultValue="DESC") String direction) {
+		Page<Order> pageObj = service.findPerPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(pageObj);
 	}
 }
--- src/main/java/com/arosseto/g2glite/services/OrderService.java
+++ src/main/java/com/arosseto/g2glite/services/OrderService.java
@@ -2,20 +2,28 @@ package com.arosseto.g2glite.services;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.arosseto.g2glite.entities.Client;
 import com.arosseto.g2glite.entities.InvoicePayment;
 import com.arosseto.g2glite.entities.Order;
 import com.arosseto.g2glite.entities.OrderItem;
 import com.arosseto.g2glite.entities.enums.OrderStatus;
 import com.arosseto.g2glite.entities.enums.PaymentStatus;
+import com.arosseto.g2glite.repositories.ClientRepository;
 import com.arosseto.g2glite.repositories.OrderItemRepository;
 import com.arosseto.g2glite.repositories.OrderRepository;
 import com.arosseto.g2glite.repositories.PaymentRepository;
+import com.arosseto.g2glite.security.UserAuth;
+import com.arosseto.g2glite.services.exceptions.AuthorizationException;
 import com.arosseto.g2glite.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -81,5 +89,16 @@ public class OrderService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		
 		return obj;
+	}
+	
+	public Page<Order> findPerPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserAuth user = UserService.authenticated();
+		if (Objects.isNull(user)) {
+			throw new AuthorizationException("Access Denied");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Client client = clientService.findById(user.getId()); 
+		
+		return repo.findByClient(client, pageRequest);
 	}
 }
