dc48580a96431d3a733b17a14d3be3222f6d3c4e
alexandrerosseto <alexandrerosseto@gmail.com>
2020-07-15 16:37:13 -0300

    Cropping image

--- src/main/java/com/arosseto/g2glite/services/ClientService.java
+++ src/main/java/com/arosseto/g2glite/services/ClientService.java
@@ -56,6 +56,9 @@ public class ClientService {
 	
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+
+	@Value("${img.profile.size}")
+	private Integer size;
 	
 	public List<Client> findAll() {
 		return repo.findAll();
@@ -140,6 +143,9 @@ public class ClientService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(file);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "iamge");
